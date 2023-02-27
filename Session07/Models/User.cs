e103b4f037d8913aafa07ae7b7f7bd72d50c0910
Pasha <pashaie@gmail.com>
2023-02-27 19:11:39 +0330

    users + migration

---
+++ Session07/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session07.Models
+{
+    public class User
+    {
+        //GUID - Cluster ID
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Boolean IsActive { get; set; }
+    }
+}
